eacf6c04ffee6a89961c3d3bef778a83e69ca637
David Ritchey <ritchey0713@gmail.com>
2018-03-18 00:54:33 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -51,7 +51,7 @@ end
   def winner(board)
     if !(won(board))
       return nil
-    elsif board[won?(board).detect {combo}]      
+    elsif board[won?(board).detect {combo} board]      
     
   end 
 end
