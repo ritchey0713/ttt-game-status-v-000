176fc29ee2d6c31d59e2fed223c27e654baceb16
David Ritchey <ritchey0713@gmail.com>
2018-03-18 00:45:44 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -52,7 +52,7 @@ end
      WIN_COMBINATIONS.detect do |combo|
       if (board[combo[0]]) == "X" && (board[combo[1]]) == "X" && (board[combo[2]]) == "X"
         return "X"
-      elsif  elsif  (board[combo[0]]) == "O" && (board[combo[1]]) == "O" && (board[combo[2]]) == "O"
+      elsif (board[combo[0]]) == "O" && (board[combo[1]]) == "O" && (board[combo[2]]) == "O"
         return "O"
       else
         return nil
