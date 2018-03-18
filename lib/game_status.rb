dec9e66d4e083a67c5c3a63c1aa10fb0446824aa
David Ritchey <ritchey0713@gmail.com>
2018-03-18 00:50:45 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -50,8 +50,10 @@ end
   
   def winner(board)
      WIN_COMBINATIONS.detect do |combo|
-      if won?(board[0]) == "X"
+      if (board[combo[0]]) == "X" && (board[combo[1]]) == "X" && (board[combo[2]]) == "X"
         return "X"
+      elsif  (board[combo[0]]) == "O" && (board[combo[1]]) == "O" && (board[combo[2]]) == "O"
+        return "O"
       else
         return nil
       
